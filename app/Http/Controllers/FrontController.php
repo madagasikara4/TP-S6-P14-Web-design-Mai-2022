719a32d825830977183dcb926fc26cba5c002d16
madagasikara4 <miaroandy25@gmail.com>
2023-05-08 09:07:40 +0300

    cache serveur

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -5,22 +5,28 @@ namespace App\Http\Controllers;
 use App\Models\Information;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class FrontController extends Controller
 {
     public function fiche($id){
         $i=explode('-',$id);
-        $article = Information::find($i[0]);
-        return view('frontfiche',[
-            'article' => $article
-        ]);
+        $index=$i[0];
+        $list=Cache::remember('fiches',300,function () use($index){
+            return Information::find($index);
+        });
+        $response = response()->view('frontfiche',['article' => $list]);
+        $response->header('Cache-Control','max-age=3600 , public ');
+        return $response;
     }
 
     public function liste(){
-        $info=Information::all();
-        return view('frontListe',[
-            'info' => $info
-        ]);
+        $list=Cache::remember('listes',300,function (){
+            return Information::all();
+        });
+        $response = response()->view('frontListe',['info' => $list]);
+        $response->header('Cache-Control','max-age=3600 , public ');
+        return $response;
     }
 
     public function recherche(Request $request){
